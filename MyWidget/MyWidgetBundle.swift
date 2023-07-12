b33dad26e3c45824481e9fe104f84559778201dd
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-07-12 16:52:24 -0500

    [add] - My first widget

---
+++ MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by Pierre Juarez U. on 12/07/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidget()
+    }
+}
