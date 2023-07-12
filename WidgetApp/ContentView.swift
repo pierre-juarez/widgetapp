9bf2164051082e15981114bd5300deea2fa8b952
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-07-12 16:29:52 -0500

    [add] - Initial commit

---
+++ WidgetApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WidgetApp
+//
+//  Created by Pierre Juarez U. on 12/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by Pierre Juarez U. on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
